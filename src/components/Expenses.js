e21f26f55721193c1260695f2dbcc81d5f3ae9b4
ahuespi <amir.huespi@gmail.com>
2022-01-19 07:41:24 -0300

    first commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+
+function Expenses(props) {
+    return (
+        <div className='expenses'>
+            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
+        </div>
+    )
+}
+
+export default Expenses
